6436b400025a05309d3e04e6b3e722084fbe6549
zero choi <ho4040@nooslab.com>
2018-02-10 03:34:46 +0900

    update

--- conditional_gan_mnist.py
+++ conditional_gan_mnist.py
@@ -1,163 +1,141 @@
-import tensorflow as tf
+import os, time, itertools, imageio, pickle
 import numpy as np
 import matplotlib.pyplot as plt
-import math
-from skimage.transform import resize
+import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
-import time
-
-def generator(z_inputs, y_inputs, reuse=False, training=True, color_channel=3):
-	
-	with tf.device('/device:GPU:0'):
-		s_size = 4
-		initializer = tf.random_normal_initializer(mean=0, stddev=0.02)
-
-		with tf.variable_scope('g', reuse=reuse):
-
-			z = tf.convert_to_tensor(inputs)
-
-			# z vector convert into tensor
-			#outputs = tf.layers.dense(z, s_size * s_size * 1024)
-			#outputs = tf.nn.relu(outputs) # shape (batch_size, 4, 4, 1024)
-			
-			# Both z and y are mapped to hidden layers with Rectified Linear Unit (ReLu) activation [4, 11]
-			# shape of z = (None, 100)
-			z_layer = tf.layers.dense(z, 200, activation=tf.nn.relu)
-			z_layer = tf.layers.dense(z_layer, 200, activation=tf.nn.relu)
-			z_layer = tf.layers.dense(z_layer, 200, activation=tf.nn.relu)
-			z_layer = tf.layers.dense(z_layer, 200, activation=tf.nn.relu)
-			
-			
-			y_layer = tf.layers.dense(y_inputs, 1000)
-			
-			
-			# tanh output
-			g = tf.nn.tanh(tconv4, name='generator') # output shape = (batch_size, 64, 64, 3)
-			#g = tf.nn.sigmoid(tconv4)
-			return g
-
-def discriminator(inputs, reuse=False, training=True):
-	
-	with tf.device('/device:GPU:0'):
-
-		initializer = tf.random_normal_initializer(mean=0, stddev=0.02)
-
-		with tf.variable_scope('d', reuse=reuse):
-			d_inputs = tf.convert_to_tensor(inputs)
-
-			# conv 1
-			conv1 = tf.layers.conv2d(d_inputs, 64, [5, 5], strides=(2, 2), padding='SAME', kernel_initializer=initializer)
-			conv1 = tf.layers.batch_normalization(conv1, training=training)
-			conv1 = tf.nn.leaky_relu(conv1) # shape (batch_size, 32, 32, 64)
-
-			# conv 2
-			conv2 = tf.layers.conv2d(conv1, 128, [5, 5], strides=(2, 2), padding='SAME', kernel_initializer=initializer)
-			conv2 = tf.layers.batch_normalization(conv2, training=training)
-			conv2 = tf.nn.leaky_relu(conv2) # shape (batch_size, 16, 16, 128)
-
-			# conv 3
-			conv3 = tf.layers.conv2d(conv2, 256, [5, 5], strides=(2, 2), padding='SAME', kernel_initializer=initializer)
-			conv3 = tf.layers.batch_normalization(conv3, training=training)
-			conv3 = tf.nn.leaky_relu(conv3) # shape (batch_size, 4, 4, 256)
-
-			# conv 4
-			conv4 = tf.layers.conv2d(conv3, 512, [5, 5], strides=(2, 2), padding='SAME', kernel_initializer=initializer)
-			conv4 = tf.layers.batch_normalization(conv4, training=training)
-			conv4 = tf.nn.leaky_relu(conv4) # shape (batch_size, 2, 2, 512)
-
-			batch_size = conv4.get_shape()[0].value
-
-			reshape = tf.reshape(conv4, [batch_size, -1])
-			d = tf.layers.dense(reshape, 1, name='d')
-			d = tf.nn.sigmoid(reshape)
-			return d
 
 
-def build_graph(z_dim = 100, batch_size = 128, test_result_num=(8,2), input_image_shape=(28,28,1)):
-	
-	img_w = input_image_shape[0]
-	img_h = input_image_shape[1]
-	color_channel = input_image_shape[2]
-	
-	with tf.device('/device:GPU:0'):
-		
-		
-		# discriminator value from fake image
-		z = tf.random_uniform([batch_size, z_dim], minval=-1.0, maxval=1.0)
-		fake_data = generator(z, reuse=False, training=True, color_channel=color_channel)
-		d_f = discriminator(fake_data, reuse=False, training=True)
-		
-		# discriminator value from real image
-		real_data_input = tf.placeholder(shape=[batch_size, None], dtype=tf.float32)		
-		real_images = tf.reshape(real_data_input, (batch_size, img_w, img_h, color_channel)) # convert raw data to valid image data
-		real_images = tf.image.resize_images(real_images, (64,64)) # change size as D input
-		d_r = discriminator(real_images, reuse=True, training=True)
-		
-		# loss of D and G
-		d_loss = -tf.reduce_mean( tf.log(d_r) + tf.log(1-d_f))  # Minus sign for Adam optimizer that only can minimize.
-		g_loss = tf.reduce_mean(tf.log(1-d_f))  
-		
-		update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS) # turn on batchorm
-		with tf.control_dependencies(update_ops):
-			opt_d = tf.train.AdamOptimizer(learning_rate=0.0002, beta1=0.5)
-			d_train = opt_d.minimize(d_loss, var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='d'))
-			opt_g = tf.train.AdamOptimizer(learning_rate=0.0002, beta1=0.5)
-			g_train = opt_g.minimize(g_loss, var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='g'))
-		
-	row = test_result_num[1]
-	col = test_result_num[0]
-	
-	gen_size = row*col
-	gen_inputs = tf.random_uniform([gen_size, z_dim], minval=-1.0, maxval=1.0)
-	gen_images = generator(gen_inputs, reuse=True, training=False, color_channel=color_channel)
-	gen_images = tf.image.convert_image_dtype(tf.div(tf.add(gen_images, 1.0), 2.0), tf.uint8)
-	gen_images = [img for img in tf.split(gen_images, gen_size, axis=0)]
-	rows = []
-	for i in range(0, row, 1):
-		rows.append(tf.concat(gen_images[col * i + 0:col * i + col], 2))
-	gen_fakes = tf.concat(rows, 1)
-	
-	
-	return g_train, d_train, d_loss, g_loss, real_data_input, gen_fakes, real_images
+# G(z)
+def generator(x, y, isTrain=True, reuse=False):
+    with tf.variable_scope('generator', reuse=reuse), tf.device('/device:GPU:0'):
+        w_init = tf.contrib.layers.xavier_initializer()
 
-def main():
-	# download mnist data
-	mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-	batch_size = 128
+        cat1 = tf.concat([x, y], 1)
 
-	g_train, d_train, d_loss, g_loss, real_data_input, gen_fakes, real_images = build_graph( z_dim = 100,  batch_size = batch_size, test_result_num=(8,2), input_image_shape=(28,28,1))
+        dense1 = tf.layers.dense(cat1, 128, kernel_initializer=w_init)
+        relu1 = tf.nn.relu(dense1)
 
-	sess = tf.Session()
-	sess.run(tf.global_variables_initializer())
-	start_time = time.time()
+        dense2 = tf.layers.dense(relu1, 784, kernel_initializer=w_init)
+        g = tf.nn.tanh(dense2)
 
-	batches = mnist.train.next_batch(batch_size)[0]
-	_fakes, _real = sess.run([gen_fakes, real_images], feed_dict={real_data_input:batches})
+        return g
 
-	#plt.axis('off')
-	# plt.figure(figsize=(8,1))	
-	# plt.subplot(121)
-	# plt.imshow(np.squeeze(np.squeeze(_real[0]*255)).astype(np.uint8), cmap='gray')
-	# plt.subplot(122)
-	# plt.imshow(np.squeeze(np.squeeze(_fakes[0])).astype(np.uint8), cmap='gray')
-	# plt.show()
+    
+# D(x)
+def discriminator(x, y, isTrain=True, reuse=False):
+    with tf.variable_scope('discriminator', reuse=reuse), tf.device('/device:GPU:0'):
+        w_init = tf.contrib.layers.xavier_initializer()
 
+        cat1 = tf.concat([x, y], 1)
 
-	k = 1
-	for i in range(50000):
-		
-		batches = mnist.train.next_batch(batch_size)[0]
-		sess.run([d_train], feed_dict={real_data_input:batches})
-		sess.run([g_train], feed_dict={real_data_input:batches})
+        dense1 = tf.layers.dense(cat1, 128, kernel_initializer=w_init)
+        lrelu1 = tf.nn.leaky_relu(dense1)
 
-		if i % 100 == 0 :
-			print("--- step %d,  %s seconds ---" % (i, time.time() - start_time))
-			_fakes = sess.run([gen_fakes], feed_dict={real_data_input:batches})
-			plt.figure(figsize=(8,4))	
-			plt.axis('off')
-			plt.tight_layout(pad=0.0, w_pad=0.0, h_pad=0.0)
-			plt.imshow(np.squeeze(_fakes[0][0]).astype(np.uint8), cmap='gray')
-			plt.show()
+        dense2 = tf.layers.dense(lrelu1, 1, kernel_initializer=w_init)
+        d = tf.nn.sigmoid(dense2)
 
+        return d, dense2
 
-main()
+onehot = np.eye(10) # make I matrix
+# load MNIST
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+train_set = (mnist.train.images - 0.5) / 0.5  # normalization; range: -1~1 from 0~1
+train_label = mnist.train.labels
+# training parameters
+batch_size = 100
+lr = 0.0002
+train_epoch = 100
+# variables : input
+x = tf.placeholder(tf.float32, shape=(None, 784))
+y = tf.placeholder(tf.float32, shape=(None, 10))
+z = tf.placeholder(tf.float32, shape=(None, 100))
+#z = tf.random_uniform([batch_size, 100], minval=-1.0, maxval=1.0)
+isTrain = tf.placeholder(dtype=tf.bool)
+
+# networks : generator
+G_z = generator(z, y, isTrain)
+# networks : discriminator
+D_real, D_real_logits = discriminator(x, y, isTrain)
+D_fake, D_fake_logits = discriminator(G_z, y, isTrain, reuse=True)
+with tf.device('/device:GPU:0'):
+    # loss for each network
+    D_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_real_logits, labels=tf.ones([batch_size, 1])))
+    D_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake_logits, labels=tf.zeros([batch_size, 1])))
+
+    #D_loss_real = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=D_real, labels=tf.ones([batch_size, 1])))
+    #D_loss_fake = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=D_fake, labels=tf.zeros([batch_size, 1])))
+
+    D_loss = D_loss_real + D_loss_fake
+    G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake_logits, labels=tf.ones([batch_size, 1])))
+
+# trainable variables for each network
+T_vars = tf.trainable_variables()
+D_vars = [var for var in T_vars if var.name.startswith('discriminator')]
+G_vars = [var for var in T_vars if var.name.startswith('generator')]
+
+# optimizer for each network
+with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+    D_optim = tf.train.AdamOptimizer(lr, beta1=0.5).minimize(D_loss, var_list=D_vars)
+    G_optim = tf.train.AdamOptimizer(lr, beta1=0.5).minimize(G_loss, var_list=G_vars)
+
+# trainable variables for each network
+T_vars = tf.trainable_variables()
+D_vars = [var for var in T_vars if var.name.startswith('discriminator')]
+G_vars = [var for var in T_vars if var.name.startswith('generator')]
+
+# optimizer for each network
+with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+    D_optim = tf.train.AdamOptimizer(lr, beta1=0.5).minimize(D_loss, var_list=D_vars)
+    G_optim = tf.train.AdamOptimizer(lr, beta1=0.5).minimize(G_loss, var_list=G_vars)    
+
+# open session and initialize all variables
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+#sess = tf.Session()
+#sess.run(tf.global_variables_initializer())
+writer = tf.summary.FileWriter('./cGAN_log', sess.graph)
+merged = tf.summary.merge_all()
+
+np.random.seed(int(time.time()))
+print('training start!')
+start_time = time.time()
+
+for epoch in range(train_epoch):
+    
+    # update discriminator
+    for iter in range(len(train_set) // batch_size):
+        
+        x_ = train_set[iter * batch_size:(iter + 1) * batch_size]
+        y_ = train_label[iter * batch_size:(iter + 1) * batch_size]
+        z_ = np.random.normal(0, 1, (batch_size, 100))
+
+        _ = sess.run([D_optim], {x: x_, y: y_, z: z_, isTrain: True})
+
+        # update generator
+        z_ = np.random.normal(0, 1, (batch_size, 100))
+        y_ = np.random.randint(0, 9, (batch_size, 1))
+        y_ = onehot[y_.astype(np.int32)].squeeze()
+        _ = sess.run([G_optim], {z: z_, x: x_, y: y_, isTrain: True})
+        
+    _summ = sess.run([merged], {z: z_, x: x_, y: y_, isTrain: True})
+    writer.add_summary(_summ[0], epoch)
+    if epoch % 10 == 0:
+        print("epoch {}".format(epoch))
+
+# TEST witht label
+
+test_num = 9
+for i in range(10):    
+    z_ = np.random.normal( 0, 1, (test_num, 100) )
+    y_ = [onehot[i]] * test_num
+    
+    _fakes = sess.run(fake_images, {z:z_, y:y_, isTrain:False})
+    _fakes = np.reshape(_fakes, (test_num,28,28))
+    
+    plt.figure(figsize=(10,1))
+    for k in range(test_num):
+        plt.subplot(''.join(str(x) for x in [1,test_num,k+1]))
+        plt.axis('off')
+        plt.imshow(_fakes[k], cmap="gray")
+    plt.show()
+        
